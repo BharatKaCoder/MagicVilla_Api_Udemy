d987d7f9b28e17278bc74facb45c9538c1394c91
khup practice <khup.practice@gmail.com>
2025-01-03 21:48:00 +0530

    Step 1 : Basic setup (section 1 to 3) controller,model,DTO

---
+++ MagicVilla_Api_Udemy/Controllers/VillaApiController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla_Api_Udemy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Api_Udemy.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/villaapi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaModel> GetVillas() 
+        {
+            return new List<VillaModel>
+            {
+                new VillaModel { Id = 1, Name = "Beach View" },
+                new VillaModel { Id = 2, Name = "Pool View" }
+            };
+
+        }
+    }
+}
---
+++ MagicVilla_Api_Udemy/Models/VillaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_Api_Udemy.Models
+{
+    public class VillaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
