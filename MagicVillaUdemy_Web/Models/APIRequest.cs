b0fbee155a230a1f3628532ff7727b55d243b619
khup practice <khup.practice@gmail.com>
2025-01-06 17:41:16 +0530

    Web Project : RequestApi file created and AutoMapper package installed

---
+++ MagicVillaUdemy_Web/Models/APIRequest.cs
@@ -0,0 +1,11 @@
+﻿using static MagicVilla_Utility.StaticDetails;
+
+namespace MagicVillaUdemy_Web.Models
+{
+    public class APIRequest
+    {
+        public APIType ApiType { get; set; } = APIType.GET;
+        public string ApiUrl { get; set; }
+        public object Data {  get; set; } 
+    }
+}
