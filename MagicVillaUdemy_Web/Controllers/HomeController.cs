339b3b744bbef818a23880dc94e649b5296be00c
khup practice <khup.practice@gmail.com>
2025-01-09 20:01:24 +0530

    Home Controller code reverted

--- MagicVillaUdemy_Web/Controllers/HomeController.cs
+++ MagicVillaUdemy_Web/Controllers/HomeController.cs
@@ -10,25 +10,42 @@ namespace MagicVillaUdemy_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IVillaService _VillService;
-        private readonly IMapper _Mapper;
+        private readonly ILogger<HomeController> _logger;
+        //private readonly IVillaService _VillService;
+        //private readonly IMapper _Mapper;
 
-        public HomeController(IVillaService villaService, IMapper mapper)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _VillService = villaService;
-            _Mapper = mapper;
+            _logger = logger;
+            //_VillService = villaService;
+            //_Mapper = mapper;
         }
 
-        public async Task<IActionResult> IndexVilla()
+        public IActionResult Index()
         {
-            List<VillaDTO> List = new();
-            var Response = await _VillService.GetAllAsync<APIResponse>();
-            if (Response != null && Response.Success)
-            {
-                List = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(Response.Result));
-            }
-            return View(List);
+            return View();
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //public async Task<IActionResult> IndexVilla()
+        //{
+        //    List<VillaDTO> List = new();
+        //    var Response = await _VillService.GetAllAsync<APIResponse>();
+        //    if (Response != null && Response.Success)
+        //    {
+        //        List = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(Response.Result));
+        //    }
+        //    return View(List);
+        //}
 
     }
 }
