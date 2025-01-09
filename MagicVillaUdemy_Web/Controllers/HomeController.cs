b7c7a5f699fff6ec54d8829dcb98b36fcc91308b
khup practice <khup.practice@gmail.com>
2025-01-09 18:42:57 +0530

    home controller cod updated

--- MagicVillaUdemy_Web/Controllers/HomeController.cs
+++ MagicVillaUdemy_Web/Controllers/HomeController.cs
@@ -1,32 +1,34 @@
 using System.Diagnostics;
+using AutoMapper;
 using MagicVillaUdemy_Web.Models;
+using MagicVillaUdemy_Web.Models.DTO;
+using MagicVillaUdemy_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MagicVillaUdemy_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IVillaService _VillService;
+        private readonly IMapper _Mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IVillaService villaService, IMapper mapper)
         {
-            _logger = logger;
+            _VillService = villaService;
+            _Mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexVilla()
         {
-            return View();
+            List<VillaDTO> List = new();
+            var Response = await _VillService.GetAllAsync<APIResponse>();
+            if (Response != null && Response.Success)
+            {
+                List = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(Response.Result));
+            }
+            return View(List);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
